f7b9acdfedc4732123b52ed132695419d39875b8
BethCries <b.jeanne.mills@gmail.com>
2023-04-20 11:04:18 -0400

    images and text successfully showing in browser

--- app.js
+++ app.js
@@ -263,7 +263,26 @@ const populateQuestions = () => {
         question.answers.forEach(answer  => {
             const answerBlock = document.createElement('div')
             answerBlock.classList.add('answer-block')
+            answerBlock.addEventListener('click', () => handleClick)
+            const answerImage = document.createElement('img')
+            answerImage.setAttribute('src', answer.image)
+            answerImage.setAttribute('alt', answer.alt)
 
+            const answerTitle = document.createElement('h3')
+            answerTitle.textContent = answer.text
+
+            // const answerInfo = document.createElement('p')
+            // const imageLink = document.createElement('a')
+            // imageLink.setAttribute('href', answer.credit)
+            // const sourceLink = document.createElement('a')
+            // sourceLink.textContent = 'Unsplash'
+            // sourceLink.setAttribute('src', 'https://www.unsplash.com')
+            // answerInfo.append(imageLink, ' to ', sourceLink)
+            // could go in answer.block (, answerInfo)
+
+            answerBlock.append(answerImage, answerTitle)
+        
+            answersBlock.append(answerBlock)
         })
         questionDisplay.append(answersBlock)
 
